8306d83d2f3fac7815525658324959610def6434
Aldison Lluka <34491203+aldisonll@users.noreply.github.com>
2022-11-07 02:38:35 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -4,8 +4,15 @@ FILE=~/../usr/etc/profile.d/mymotd.sh
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
+
+apt-get install neofetch &> /dev/null
+if [ $? -eq 0 ]; then
+    neofetch_command="bash neofetch"
+fi
+
 echo "#!/bin/bash
-bash .custom_bashrc_generator" >> $FILE
+bash .custom_bashrc_generator
+$neofetch_command" >> $FILE
 
 chmod +x $FILE
 echo "Installed, please restart terminal"
